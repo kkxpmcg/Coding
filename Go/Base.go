963e83a7235219f6245398c5844fd8e0aba2fb30
kkxpmcg <kkxpmcg@gamail.com>
2023-06-01 19:10:06 +0800

    go

---
+++ Go/Base.go
@@ -0,0 +1,22 @@
+// 单行注释
+/*多行注释
+  多行注释
+*/
+
+package main
+
+import "fmt"
+
+func test() {
+	fmt.Print("Google" + "Runoob")
+}
+
+var x int
+
+const Pi float64 = 3.1415926
+
+/* go中的关键字
+break,default,func,interface,select,case,defer,go, map, struct. chan, else, goto, package, switch,
+const, fallthough, if, range, type, continue, for, import, return, var
+
+*/
---
+++ Go/HelloWorld.go
@@ -0,0 +1,17 @@
+// 包声明
+package main
+
+//引入包
+import "fmt"
+
+//函数
+func main() {
+	fmt.Println("hello,world!")
+
+	test()
+}
+
+// 变量第一个字母大写可以被外部访问
+// 变量第一个字母小写对包外是不可见的
+
+// 文件名和包名可以不一致
